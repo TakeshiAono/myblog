be47eee511d2e9cb11674cd6ec75f6a04fcf68a5
aono <takeshi.aono001@gmail.com>
2022-05-17 16:51:34 +0900

    演習提出 ver1

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,58 +1,45 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy ]
 
-  # GET /blogs or /blogs.json
+
   def index
     @blogs = Blog.all
   end
 
-  # GET /blogs/1 or /blogs/1.json
   def show
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
-
-    
-
   end
 
-  # GET /blogs/1/edit
   def edit
-
   end
 
   def create
     @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blogs_path, notice: "投稿しました"      
-    else
-      redirect_to blogs_path, notice: "投稿失敗しました"
-    end
     
+    if params[:back]
+      render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "投稿しました"      
+      else
+        @error = "投稿失敗しました"
+        render "confirm"
+      end      
+    end    
   end
 
-  # POST /blogs or /blogs.json
   def confirm
-
     @blog = Blog.new(blog_params)
-
-    # @blog = Blog.find(4)
-
-      # if @blog.save
-
-      # else
-
-      # end
-      # redirect_to blogs_path
   end
 
   # PATCH/PUT /blogs/1 or /blogs/1.json
   def update
     @blog = Blog.find(params[:id])
-    acheive = @blog.update(blog_params)
 
+    acheive = @blog.update(blog_params)
     if acheive === true 
       redirect_to blogs_path, notice: "更新成功"
     else
@@ -60,7 +47,6 @@ class BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1 or /blogs/1.json
   def destroy
     @blog.destroy
 
@@ -71,12 +57,11 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_blog
       @blog = Blog.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def blog_params
       params.require(:blog).permit(:title, :content)
     end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
   
+  
   validates :title, :content, presence: true
   validates :content, length: {in:1..140}
 end
